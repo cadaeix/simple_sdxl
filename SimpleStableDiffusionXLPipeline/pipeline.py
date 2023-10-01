a470938a3fabdd8dc3857a7417e8bb96708a92e1
cadaeix <cadaeix@gmail.com>
2023-10-01 13:59:50 +1100

    Revert "try and fix lora img2img"
    This reverts commit c4f2d85e22f7b271658ae0194bc004533f7c9b71.

--- SimpleStableDiffusionXLPipeline/pipeline.py
+++ SimpleStableDiffusionXLPipeline/pipeline.py
@@ -40,8 +40,7 @@ from diffusers.models.attention_processor import (
 from diffusers.schedulers import KarrasDiffusionSchedulers
 from diffusers.utils.torch_utils import randn_tensor
 from diffusers.utils import (
-    scale_lora_layers,
-    unscale_lora_layers,
+    is_invisible_watermark_available,
     is_accelerate_available,
     is_accelerate_version,
     logging,
@@ -338,12 +337,8 @@ class StableDiffusionXLPipeline(
             self._lora_scale = lora_scale
 
             # dynamically adjust the LoRA scale
-            if not self.use_peft_backend:
-                adjust_lora_scale_text_encoder(self.text_encoder, lora_scale)
-                adjust_lora_scale_text_encoder(self.text_encoder_2, lora_scale)
-            else:
-                scale_lora_layers(self.text_encoder, lora_scale)
-                scale_lora_layers(self.text_encoder_2, lora_scale)
+            adjust_lora_scale_text_encoder(self.text_encoder, lora_scale)
+            adjust_lora_scale_text_encoder(self.text_encoder_2, lora_scale)
 
         prompt = [prompt] if isinstance(prompt, str) else prompt
 
@@ -517,11 +512,6 @@ class StableDiffusionXLPipeline(
             negative_pooled_prompt_embeds = negative_pooled_prompt_embeds.repeat(
                 1, num_images_per_prompt
             ).view(bs_embed * num_images_per_prompt, -1)
-
-        if isinstance(self, StableDiffusionXLLoraLoaderMixin) and self.use_peft_backend:
-            # Retrieve the original scale by scaling back the LoRA layers
-            unscale_lora_layers(self.text_encoder)
-            unscale_lora_layers(self.text_encoder_2)
 
         return (
             prompt_embeds,
